651a28317a1b36e6451bb8cfacc90f805e541fcb
gksehfapd <1ehfapd@gmail.com>
2023-07-24 22:05:35 +0900

    feat: github login

---
+++ pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,13 @@
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+
+export const authOptions = {
+	providers: [
+		GithubProvider({
+			clientId: '007c24982ddb1a720d3c',
+			clientSecret: '0ef86ac8d2f12265583a7d998e7ca8e409bb413c'
+		})
+	],
+	secret: 'dkssudgktpdygksehdauddlqslek.'
+}
+export default NextAuth(authOptions)

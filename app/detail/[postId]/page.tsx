b8599c6ae94764c42bb9268cd795bfef08ed9519
gksehfapd <1ehfapd@gmail.com>
2023-07-28 15:29:06 +0900

    fix: 로그인 하지 않았을때 게시물 보이지 않는 버그 수정

--- app/detail/[postId]/page.tsx
+++ app/detail/[postId]/page.tsx
@@ -12,15 +12,7 @@ export default async function Detail(props: any) {
 	const db = (await connectDB).db('forum')
 	const postId = props.params.postId
 	let result = await db.collection('post').findOne({ _id: new ObjectId(`${postId}`) })
-
-	let likeList: any
-
-	//로그인 하지 않았을때
-	if (session === null) {
-		likeList = null
-	} else {
-		likeList = await db.collection('likes').findOne({ userEmail: session.user.email })
-	}
+	let likeList = await db.collection('likes').findOne({ userEmail: session.user.email })
 
 	if (result === null) {
 		return notFound()
@@ -29,7 +21,7 @@ export default async function Detail(props: any) {
 	return (
 		<div>
 			<h4>상세페이지</h4>
-			<Like postId={postId} likeList={likeList === null ? [] : likeList.postId} />
+			<Like postId={postId} likeList={likeList.postId} />
 
 			<h4>{result?.title}</h4>
 			<p>{result?.content}</p>
